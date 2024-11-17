57c5e164b15d1b46295dac3ff23476b062b4d440
Aaron Obed Girón <aogarrido.g@gmail.com>
2024-11-17 17:11:19 -0600

    Prime ng table

--- src/app/products/pages/order/order.component.ts
+++ src/app/products/pages/order/order.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { interval } from 'rxjs';
+import { Color, Hero } from '../../interfaces/hero.interface';
 
 @Component({
   selector: 'products-order',
@@ -9,6 +11,36 @@ import { Component } from '@angular/core';
 export class OrderComponent {
 
   public isUppercase: boolean = false;
+
+  public files: number = 1;
+
+  public heroes : Hero[] = [
+    {
+      name:'Superman',
+      cantFly: true,
+      color:Color.blue
+    },
+    {
+      name:'Batman',
+      cantFly: false,
+      color:Color.black
+    },
+    {
+      name:'DareDevil',
+      cantFly: false,
+      color:Color.red
+    },
+    {
+      name:'Robin',
+      cantFly: false,
+      color:Color.red
+    },
+    {
+      name:'Linterna Verde',
+      cantFly: true,
+      color:Color.green
+    }
+  ];
 
   toggleUperCase(){
     this.isUppercase = !this.isUppercase;
